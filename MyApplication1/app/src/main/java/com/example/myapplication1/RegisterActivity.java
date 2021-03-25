67ac2c10016eaf4f8d7662281ba760273fb99ac8
P-SG <71485469+P-SG@users.noreply.github.com>
2021-03-25 16:59:22 +0900

    Update RegisterActivity.java

--- MyApplication1/app/src/main/java/com/example/myapplication1/RegisterActivity.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/RegisterActivity.java
@@ -19,7 +19,7 @@ public class RegisterActivity extends AsyncTask<String, Void, String> {
             String str;
             //주석
             // 접속할 서버 주소 (이클립스에서 android.jsp 실행시 웹브라우저 주소)
-            URL url = new URL("http://192.168.219.109:8090/Android_Web/androidDB.jsp");
+            URL url = new URL("http://ip:port/Android_Web/androidDB.jsp");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
